6aa0d5e0ba201200dfedcfafa4ef858e297a25e9
SeanLee17 <taishanlee17@gmail.com>
2016-05-20 14:12:07 -0700

    Update Pikumen.java

--- Pikumen.java
+++ Pikumen.java
@@ -1,4 +1,4 @@
-package Pikamen;
+package Pikumen;
 import java.util.ArrayList;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
@@ -17,6 +17,8 @@ public class Pikumen {
 	private String name;
 	private int level;
 	private int exp;
+	private int pokedexNum; // for differentiating pikumen from each other and comparing easily
+	private int type; //water, fire, grass type
 	private Image image;
 	private float hpPLvl;
 	private float atkPLvl;
@@ -29,12 +31,14 @@ public class Pikumen {
 	private float spdPLvl;
 	private int startSpd;
 	
-	public Pikumen(String name, int level, int exp, Image image, float hpPLvl, float atkPLvl, 
+	public Pikumen(String name, int level, int exp, int pokedexNum, int type, Image image, float hpPLvl, float atkPLvl, 
 					float defPLvl, int startHp, int startAtk, int startDef, int[] startingMoves,
 					float spdPLvl, int startSpd){
 		this.name = name;
 		this.level = level;			// or does level = a certain point of experience?
 		this.exp = exp;
+		this.pokedexNum = pokedexNum;
+		this.type = type;
 		this.image = image;
 		this.hpPLvl = hpPLvl;
 		this.atkPLvl = atkPLvl;
