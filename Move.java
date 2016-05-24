239eb76d0eecb9c5213ff24b888a88cab32b2491
SeanLee17 <taishanlee17@gmail.com>
2016-05-24 13:55:08 -0700

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,30 +1,23 @@
-package Pikamen;
-import java.util.ArrayList;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
-import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.geom.Vector2f;
+package main;
 
-
-public abstract class Move {
-	private String description;
-	private String name;
-	private int pp;
-	private int type; //fire, grass, water
-	
-	public Move(String description, String name, int pp, int type) {
-		this.description = description;
-		this.name = name;
+public class Move { 
+	protected String description;
+	protected String name; 
+ 	protected int pp; 
+	protected int type; //fire, grass, water 
+ 	 
+ 	public Move(String name, String description, int pp, int type) { 
+ 		this.description = description;
+ 		this.name = name; 
 		this.pp = pp;
 		this.type = type;
 	}
-	
-	public abstract void execMove();
-}
+ 	
+ 	public String toString(){
+ 		return (name + " " + description + " " + pp + " " + type);
+ 	}
+ 	
+	public void execMove(){
+		return;
+	}
+} 
