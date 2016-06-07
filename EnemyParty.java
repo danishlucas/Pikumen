0f8a3dc3c73fa1b51e36c345a8fbcac65a05e5d9
SeanLee17 <taishanlee17@gmail.com>
2016-06-07 13:33:12 -0700

    Update EnemyParty.java

--- EnemyParty.java
+++ EnemyParty.java
@@ -3,16 +3,22 @@ package PikumenList;
 public class EnemyParty  extends Party{
 	private static EnemyParty instance = null;
 	private int num;
-	protected EnemyParty(Pc storage) {
+	private int wildOrOpp; //0 = opponent(uncatchable), 1= wild (catchable)
+	protected EnemyParty(Pc storage, int which) {
 		super(storage);
 		num = 0;
+		wildOrOpp = which;
 	}
 		      // Exists only to defeat instantiation.
 		   
-	public static EnemyParty getInstance() {
+	public static EnemyParty getInstance(int i) {
 		if(instance == null) {
-			instance = new EnemyParty(Pc.getInstance());
+			instance = new EnemyParty(Pc.getInstance(), i);
 		 }
 		return instance;
+	}
+	
+	public int getOppOrWild(){
+		return wildOrOpp;
 	}
 }
